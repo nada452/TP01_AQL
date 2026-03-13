9c085fd8eabc31d1e03395644bf0f576515775de
Nada <nada.bel.twee@gmail.com>
2026-03-13 21:45:08 +0100

    exo3

--- src/test/java/ConditionCoverageTest/Exo3.java
+++ src/test/java/ConditionCoverageTest/Exo3.java
@@ -1,4 +1,89 @@
-package test.java.ConditionCoverageTest;
+package tast.java.ConditionCoverageTest;
+
+import main.java.BinarySearch;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class Exo3 {
+
+
+    @Test
+    void testC1_true_nullArray() {
+        assertThrows(NullPointerException.class, () ->
+                BinarySearch.binarySearch(null, 5)
+        );
+    }
+
+
+    @Test
+    void testC1_false_notNull() {
+        assertDoesNotThrow(() ->
+                BinarySearch.binarySearch(new int[]{1, 3, 5}, 3)
+        );
+    }
+
+
+
+
+    @Test
+    void testC2_false_emptyArray() {
+        assertEquals(-1, BinarySearch.binarySearch(new int[]{}, 5));
+    }
+
+
+    @Test
+    void testC2_true_enterLoop() {
+        assertEquals(0, BinarySearch.binarySearch(new int[]{5}, 5));
+    }
+
+
+    @Test
+    void testC2_true_then_false() {
+        assertEquals(-1, BinarySearch.binarySearch(new int[]{1, 3, 5, 7, 9}, 6));
+    }
+
+    @Test
+    void testC3_true_foundAtMid() {
+        assertEquals(2, BinarySearch.binarySearch(new int[]{1, 3, 5, 7, 9}, 5));
+    }
+
+
+    @Test
+    void testC3_false_notAtMid() {
+        assertEquals(4, BinarySearch.binarySearch(new int[]{1, 3, 5, 7, 9}, 9));
+    }
+
+
+
+    @Test
+    void testC4_true_goRight() {
+        assertEquals(3, BinarySearch.binarySearch(new int[]{1, 3, 5, 7, 9}, 7));
+    }
+
+
+    @Test
+    void testC4_false_goLeft() {
+        assertEquals(1, BinarySearch.binarySearch(new int[]{1, 3, 5, 7, 9}, 3));
+    }
+
+
+
+
+    @Test
+    void testFoundFirstTry() {
+        assertEquals(1, BinarySearch.binarySearch(new int[]{1, 5, 9}, 5));
+    }
+
+
+    @Test
+    void testFoundAfterGoingRight() {
+        assertEquals(2, BinarySearch.binarySearch(new int[]{1, 3, 9}, 9));
+    }
+
+
+    @Test
+    void testFoundAfterGoingLeft() {
+        assertEquals(0, BinarySearch.binarySearch(new int[]{1, 3, 9}, 1));
+    }
 }
